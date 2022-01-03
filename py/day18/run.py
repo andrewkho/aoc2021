af1793da00c1656d4743c2a7846d2a4293a3cf1e
andrewkho <andrew.kenneth.ho@gmail.com>
2022-01-02 17:09:34 -0800

    move python to py folder

--- py/day18/run.py
+++ py/day18/run.py
@@ -31,14 +31,14 @@ def main(
         x = Snum(x, snums[i])
         reduce(x)
 
-    print('1:', x.magnitude(), time.time() - t0, 's', x)
+    print('1:', magnitude(x), time.time() - t0, 's', x)
 
     t1 = time.time()
     max_mag = 0
     for i, j in itertools.product(range(len(snums)), range(len(snums))):
         x = Snum(parse(lines[i]), parse(lines[j]))
         reduce(x)
-        mag = x.magnitude()
+        mag = magnitude(x)
         if mag > max_mag:
             max_mag = mag
 
@@ -75,14 +75,12 @@ class Snum:
             self.r = None
             self.val = 0
             return lval, rval
-
         x = self.l.explode(depth+1)
         if x:
             lval, rval = x
             if rval:
                 self.r.add(rval, 'l')
             return lval, 0
-
         x = self.r.explode(depth+1)
         if x:
             lval, rval = x
@@ -90,39 +88,74 @@ class Snum:
                 self.l.add(lval, 'r')
             return 0, rval
 
-    def split(self):
-        if self.is_leaf():
-            if self.val > 9:
-                self.l = Snum(val=self.val//2)
-                self.r = Snum(val=(self.val+1)//2)
-                self.val = None
-                return True
-            else:
-                return False
-
-        if self.l.split():
-            return True
-
-        if self.r.split():
-            return True
-
-        return False
-
-    def magnitude(self):
-        if self.is_leaf():
-            return self.val
-        else:
-            return 3*self.l.magnitude() + 2*self.r.magnitude()
-
     def __str__(self):
         return str(self.val) if self.is_leaf() else f'[{str(self.l)},{str(self.r)}]'
 
 
+def explode(x):
+    depth = 0
+    stack = list()
+    cur = x
+    prevprev, prev, rval = None, None, None
+    exploded = False
+    while cur or len(stack):
+        if cur:
+            stack.append((cur, depth))
+            cur = cur.l
+            depth += 1
+        else:
+            cur, depth = stack.pop()
+            if cur.is_leaf():
+                if rval is not None:
+                    cur.val += rval
+                    return True
+                prevprev = prev
+                prev = cur
+            elif depth == 4:
+                lval, rval = cur.l.val, cur.r.val
+                cur.l, cur.r = None, None
+                cur.val = 0
+                exploded = True
+                if prevprev:
+                    prevprev.val += lval
+
+            cur = cur.r
+            depth += 1
+
+    return exploded
+
+
+def split(node):
+    stack = list()
+    cur = node
+    while cur or len(stack):
+        if cur:
+            stack.append(cur)
+            cur = cur.l
+        else:
+            cur = stack.pop()
+            if cur.is_leaf() and cur.val > 9:
+                cur.l = Snum(val=cur.val // 2)
+                cur.r = Snum(val=(cur.val + 1) // 2)
+                cur.val = None
+                return True
+            cur = cur.r
+
+    return False
+
+
+def magnitude(x):
+    if x.is_leaf():
+        return x.val
+    else:
+        return 3 * magnitude(x.l) + 2 * magnitude(x.r)
+
+
 def reduce(x):
     while True:
-        if x.explode():
+        if explode(x):
             continue
-        if x.split():
+        if split(x):
             continue
         break
 
