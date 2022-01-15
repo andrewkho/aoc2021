84b6c22d39c346077d9b2b1ce45f10e308d965a7
andrewkho <andrew.kenneth.ho@gmail.com>
2022-01-14 21:24:54 -0800

    day21 go

---
+++ go/day21.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"runtime/debug"
+	"strconv"
+	"time"
+
+	"./util"
+)
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(string(debug.Stack()))
+		}
+	}()
+
+	var filename string = os.Args[1]
+	t0 := time.Now()
+	fmt.Printf("Reading from %s,", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var init [2]int
+	re := regexp.MustCompile(`.* ([0-9].*)$`)
+	for i := 0; scanner.Scan(); i++ {
+		line := scanner.Text()
+		groups := re.FindStringSubmatch(line)
+		if groups == nil {
+			log.Panicln(line)
+		}
+		pos, err := strconv.Atoi(groups[1])
+		if err != nil {
+			log.Panicln(err)
+		}
+		init[i] = pos
+	}
+	if err := scanner.Err(); err != nil {
+		log.Panicln(err)
+	}
+	fmt.Printf(" took %v\n", time.Since(t0))
+
+	t1 := time.Now()
+	gameState := GameState{pos: init}
+	rolls := 0
+	player := 0
+	for gameState.score[0] < 1000 && gameState.score[1] < 1000 {
+		dice := rolls % 100 + 1
+		gameState = gameState.move(player, 3*dice+3)
+		rolls += 3
+		player = 1 - player
+	}
+	t := util.Min(gameState.score[0], gameState.score[1])*rolls
+	fmt.Printf("1: %v, %v\n", t, time.Since(t1))
+
+	t2 := time.Now()
+	
+	var roller [10]int
+	for i := 1; i<4; i++ {
+		for j := 1; j<4; j++ {
+			for k := 1; k<4; k++ {
+				roller[i+j+k]++
+			}
+		}
+	}
+	var winners [2]int
+	player = 0
+	states := make(map[GameState]int)
+	states[GameState{pos: init}] = 1
+	for len(states) > 0 {
+		newStates := make(map[GameState]int)
+		for state, n := range states {
+			for i, m := range roller[3:] {
+				newState := state.move(player, i+3)
+				if newState.score[player] >= 21 {
+					winners[player] += n*m
+				} else {
+					newStates[newState] += n*m
+				}
+			}
+		}
+		states = newStates
+		player = 1 - player
+	}
+	t = util.Max(winners[0], winners[1])
+	fmt.Printf("2: %v, %v\n", t, time.Since(t2))
+}
+
+type GameState struct {
+	pos [2]int
+	score [2]int
+}
+
+func (state GameState) move(player int, steps int) GameState {
+	state.pos[player] += steps
+	state.pos[player] = (state.pos[player] - 1) % 10 + 1
+	state.score[player] += state.pos[player]
+	return state
+}
--- py/day21.py
+++ py/day21.py
@@ -13,14 +13,11 @@ import re
 import fire
 
 
-def main(
-        #infile: str='test_input.txt',
-        infile: str = 'input.txt',
-):
+def main(infile: str):
     print('hi!')
 
     lines = []
-    with open(f'../inputs/day21/{infile}', 'r') as f:
+    with open(infile, 'r') as f:
         for i, line in enumerate(f.readlines()):
             if i == 0:
                 p1_0 = int(line.strip()[-2:])
@@ -29,74 +26,63 @@ def main(
 
             lines.append(line)
 
-    def advance(pos, steps):
-        pos += steps
-        if pos > 10:
-            pos = (pos - 1) % 10 + 1
-        return pos
-
     t0 = time.time()
 
-    s1, s2 = 0, 0
-    p1 = p1_0
-    p2 = p2_0
-    turns = 0
-    die = 0
-    while s1 < 1000 and s2 < 1000:
-        steps = 0
-        for i in range(3):
-            die = die+1 if die < 100 else 1
-            steps += die
+    state = State((p1_0, p2_0), (0, 0))
+    player = 0
+    rolls = 0
+    while state.score[0] < 1000 and state.score[1] < 1000:
+        die = rolls % 100 + 1
+        steps = 3*die + 3
+        state = state.advance(player, steps)
 
-        if turns % 2 == 0:
-            p1 = advance(p1, steps)
-            s1 += p1
-        else:
-            p2 = advance(p2, steps)
-            s2 += p2
+        rolls += 3
+        player = 1 - player
 
-        turns += 1
-
-    print('1:', turns, s1, 3*turns*s1, s2, 3*turns*s2, time.time() - t0, 's')
+    print('1:', rolls*min(state.score), time.time() - t0, 's')
 
     t1 = time.time()
     outcomes = defaultdict(lambda: 0)
     for i in itertools.product(range(1, 4), range(1, 4), range(1, 4)):
         outcomes[sum(i)] += 1
 
-    h1_winners = 0
-    h2_winners = 0
-    hist = defaultdict(lambda: 0, {State(p1_0, 0, p2_0, 0): 1})
+    winners = [0, 0]
+    player = 0
+    hist = defaultdict(lambda: 0, {State(pos=(p1_0, p2_0), score=(0, 0)): 1})
     while len(hist) > 0:
         new_hist = defaultdict(lambda: 0)
 
         for k, v in hist.items():
             for i, n in outcomes.items():
-                pos1 = advance(k.pos1, i)
-                scr1 = k.score1 + pos1
-                if scr1 >= 21:
-                    h1_winners += v*n
+                state = k.advance(player, i)
+                if state.score[player] >= 21:
+                    winners[player] += v*n
                 else:
-                    for j, m in outcomes.items():
-                        pos2 = advance(k.pos2, j)
-                        scr2 = k.score2 + pos2
-                        if scr2 >= 21:
-                            h2_winners += v*n*m
-                        else:
-                            new_hist[State(pos1, scr1, pos2, scr2)] += v*n*m
-
+                    new_hist[state] += v*n
         hist = new_hist
+        player = 1 - player
 
-    print('2:', h1_winners, h2_winners, time.time() - t1, 's', max([h1_winners, h2_winners]))
+    print('2:', winners, time.time() - t1, 's', max(winners))
 
 
 @dataclass(frozen=True)
 class State:
-    pos1: int
-    score1: int
-    pos2: int
-    score2: int
+    pos: Tuple[int, int]
+    score: Tuple[int, int]
 
+    def advance(self, player, steps) -> 'State':
+        pos = (self.pos[player]+steps-1) % 10 + 1
+        score = self.score[player] + pos
+        if player == 0:
+            return State(
+                (pos, self.pos[1]),
+                (score, self.score[1])
+            )
+        else:
+            return State(
+                (self.pos[0], pos),
+                (self.score[0], score),
+            )
 
 if __name__ == '__main__':
     fire.Fire(main)
